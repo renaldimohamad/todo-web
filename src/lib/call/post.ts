b84f10252ea8882bb50bcb1d629cf649fd7e9b9a
Renaldi Mohamad <sundusardian@gmail.com>
2024-12-14 19:26:41 +0700

    chore(create update-post)

--- src/lib/call/post.ts
+++ src/lib/call/post.ts
@@ -12,8 +12,20 @@ export const getAllpost = async () => {
    return res.data
 }
 
-export const getPostById = async (id: string) => {
+export const getPostById = async (id: number) => {
    const res = await api.get(`/posts/${id}`)
+
+   return res.data
+}
+
+export const updatePost = async (id: number, body: IPostForm) => {
+   const res = await api.put(`/posts/${id}`, body)
+
+   return res.data
+}
+
+export const removePost = async (id: number) => {
+   const res = await api.delete(`/posts/${id}`)
 
    return res.data
 }
