c13b85f4c7d756929212f4f2fc1111f0fb476a9f
Sundus <sundusardian@gmail.com>
2024-12-14 17:52:17 +0700

    chore(add post fitur)

--- src/stores/index.tsx
+++ src/stores/index.tsx
@@ -1,5 +1,6 @@
 import {createContext, useState} from "react"
 import {IUser, TStore} from "../services/types/store"
+import {api} from "../lib"
 
 interface StoreProps {
    children: React.ReactNode
@@ -16,6 +17,7 @@ export const StoreProvider: React.FC<StoreProps> = ({children}) => {
 
    const [isLogin, setisLogin] = useState(false)
    const [isDarkMode, setIsDarkMode] = useState(false)
+   const [posts, setPosts] = useState([])
 
    const setUser = (user: IUser) => {
       setUserState(user)
@@ -37,10 +39,31 @@ export const StoreProvider: React.FC<StoreProps> = ({children}) => {
       setIsDarkMode((Prev) => !Prev)
    }
 
+   const getPosts = async () => {
+      try {
+         const res = await api.get("/posts")
+
+         console.log(res.data)
+
+         setPosts(res.data)
+      } catch (error) {
+         console.log(error)
+      }
+   }
+
    return (
       <>
          <Store.Provider
-            value={{user, isLogin, setUser, clearUser, isDarkMode, toggleTheme}}
+            value={{
+               user,
+               isLogin,
+               setUser,
+               clearUser,
+               isDarkMode,
+               toggleTheme,
+               posts,
+               getPosts,
+            }}
          >
             {children}
          </Store.Provider>
