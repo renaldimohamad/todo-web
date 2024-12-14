2406c6b221dceb4bd2473d26dd6c65df4bb3de73
Sundus <sundusardian@gmail.com>
2024-12-14 17:51:30 +0700

    chore(add post)

--- src/services/types/store.d.ts
+++ src/services/types/store.d.ts
@@ -1,10 +1,12 @@
 export interface IStoreState {
    user: IUser
    isLogin: boolean
+   posts: IPostModel[]
 }
 
 export interface IStoreAction {
    setUser: (user: IUser) => void
+   getPosts: () => Promise<void>
    clearUser: () => void
    isDarkMode: boolean
    toggleTheme: () => void
