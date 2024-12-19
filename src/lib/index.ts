5470612514314983884032a7f7df7a59aa367ce9
Renaldi Mohamad <sundusardian@gmail.com>
2024-12-19 20:45:09 +0700

    fix(fix intergration)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-   baseURL: import.meta.env.VITE_API_URL || "http://localhost:2000",
+   baseURL: import.meta.env.VITE_API_URL || "https://to-do-list-api-gamma.vercel.app",
 })
 
 export const setAuthToken = (token: string) => {
